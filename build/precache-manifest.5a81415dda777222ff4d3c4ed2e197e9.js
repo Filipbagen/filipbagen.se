89be09d3b47893ed96145b82c8741fffda6a38d2
Filip Malm-Bägén <Filip.bagen@gmail.com>
2022-12-18 11:36:28 +0100

    initial commit

---
+++ build/precache-manifest.5a81415dda777222ff4d3c4ed2e197e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "421d816af5669886f655",
+    "url": "/filipbagen/static/css/main.98a68e50.chunk.css"
+  },
+  {
+    "revision": "421d816af5669886f655",
+    "url": "/filipbagen/static/js/main.421d816a.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/filipbagen/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/filipbagen/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "f22ad6eacb1ed915e6db",
+    "url": "/filipbagen/static/js/runtime~main.f22ad6ea.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/filipbagen/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b74b98cd3f7fad739ecfff9429f3b966",
+    "url": "/filipbagen/index.html"
+  }
+];
